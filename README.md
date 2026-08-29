319721714050cff588a55c30fca3337749700fb8
Doris <Geiniqiaohetao18@yeah.net>
2026-08-29 23:55:47 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# soli
-辅助工具
+# Soli · 夏日奇冰 v2
+
+这是独立于 Rirou 的 Soli 项目。
+
+## 部署结构
+请把整个 `Soli_Summer_Ice_v2` 文件夹中的内容作为网站根目录部署：
+- index.html
+- manifest.json
+- service-worker.js
+- css/
+- js/
+- icons/
+
+不要只上传 index.html。
+
+## Safari 添加到主屏幕
+在 Safari 打开部署地址后，使用“共享”菜单中的“添加到主屏幕”。
+项目已经包含 Apple Touch Icon，并设置了 iOS PWA 所需的 meta。
+
+## 如果仍看到旧页面
+Safari 可能保留旧的 Service Worker。先关闭旧的主屏幕网页，再在 Safari 中重新打开部署地址；必要时清除该站点的网站数据后重新部署。
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,21 @@
-const CACHE="soli-summer-ice-v1";
-const ASSETS=["./","./index.html","./manifest.json","./css/theme.css","./css/app.css","./js/store.js","./js/icons.js","./js/engine.js","./js/app.js"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("fetch",e=>e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request))));
+const CACHE="soli-summer-ice-v2";
+const ASSETS=[
+"./","./index.html","./manifest.json",
+"./css/theme.css","./css/app.css",
+"./js/store.js","./js/icons.js","./js/engine.js","./js/app.js",
+"./icons/soli-icon-192.png","./icons/soli-icon-512.png","./icons/apple-touch-icon.png"
+];
+self.addEventListener("install",event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
+});
+self.addEventListener("activate",event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener("fetch",event=>{
+  if(event.request.method!=="GET") return;
+  event.respondWith(fetch(event.request).then(response=>{
+    const copy=response.clone();
+    caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+    return response;
+  }).catch(()=>caches.match(event.request)));
+});
